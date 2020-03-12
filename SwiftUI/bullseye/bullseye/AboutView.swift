b21fd9ad4d9d5c878bef82fa49000ba4ab53dd59
ZanyZephyr <liyizhezhe@gmail.com>
2020-03-12 11:09:33 +0800

    end

--- SwiftUI/bullseye/bullseye/AboutView.swift
+++ SwiftUI/bullseye/bullseye/AboutView.swift
@@ -9,16 +9,47 @@
 import SwiftUI
 
 struct AboutView: View {
-    var body: some View {
-        
-        VStack {
-            Text("🎯 Bullseye 🎯")
-            Text("This is Bullseye, the game where you can win points and earn fame by dragging a slider.")
-            Text("Your goal is to place the slider as close as possible to the target value. The closer you are, the more points you score.")
-            Text("Enjoy!")
+    
+    let backgroundColor = Color(red: 255.0 / 255.0, green: 214.0 / 255.0, blue: 179.0 / 255.0)
+    
+    struct HeadingStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .foregroundColor(Color.black)
+                .font(Font.custom("Arial Rounded MT Bold", size: 30))
+                .padding([.top, .bottom], 20)
         }
-    .navigationBarTitle(Text("About Bullseye"))
     }
+    
+    struct TextViewStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .foregroundColor(Color.black)
+                .font(Font.custom("Arial Rounded MT Bold", size: 16))
+                .padding([.leading, .trailing], 60)
+                .padding(.bottom, 20)
+        }
+    }
+    
+    var body: some View {
+        Group{
+            VStack {
+                Text("🎯 Bullseye 🎯").modifier(HeadingStyle())
+                Text("This is Bullseye, the game where you can win points and earn fame by dragging a slider.")
+                    .modifier(TextViewStyle())
+                Text("Your goal is to place the slider as close as possible to the target value. The closer you are, the more points you score.")
+                    .modifier(TextViewStyle())
+                Text("Enjoy!")
+                    .modifier(TextViewStyle())
+            }
+            .navigationBarTitle(Text("About Bullseye"))
+            
+            .background(backgroundColor)
+        }
+        .background(Image("Background"), alignment: .center)
+
+    }
+    
 }
 
 struct AboutView_Previews: PreviewProvider {
