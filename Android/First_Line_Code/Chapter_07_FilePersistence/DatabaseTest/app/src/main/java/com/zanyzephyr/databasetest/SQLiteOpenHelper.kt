8e5098e2b0cdcec78bf018aaaba435c085ab8dcf
ZanyZephyr <liyizhezhe@gmail.com>
2020-04-17 17:31:55 +0800

    第7章完

--- Android/First_Line_Code/Chapter_07_FilePersistence/DatabaseTest/app/src/main/java/com/zanyzephyr/databasetest/SQLiteOpenHelper.kt
+++ Android/First_Line_Code/Chapter_07_FilePersistence/DatabaseTest/app/src/main/java/com/zanyzephyr/databasetest/SQLiteOpenHelper.kt
@@ -3,6 +3,7 @@ package com.zanyzephyr.databasetest
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
 import android.widget.Toast
 
 class SQLiteOpenHelper(private val context: Context, name: String, version: Int) :
@@ -13,7 +14,8 @@ class SQLiteOpenHelper(private val context: Context, name: String, version: Int)
             "author text," +
             "price real," +
             "page integer," +
-            "name text)"
+            "name text," +
+            "category_id integer)"
 
     private val createCategory = "create table Category(" +
             "id integer primary key autoincrement," +
@@ -21,14 +23,18 @@ class SQLiteOpenHelper(private val context: Context, name: String, version: Int)
             "category_code integer)"
 
     override fun onCreate(db: SQLiteDatabase?) {
+        Log.i("MainActivity", "onCreate")
         db?.execSQL(this.createBook)
         db?.execSQL(this.createCategory)
-        Toast.makeText(context, "Create succeeded", Toast.LENGTH_LONG).show()
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int){
-        db?.execSQL("drop table if exists Book")
-        db?.execSQL("drop table if exists Category")
-        onCreate(db)
+        Log.i("MainActivity", "onUpgrade")
+        if (oldVersion <= 1) {
+            db?.execSQL(createCategory)
+        }
+        if (oldVersion <= 2) {
+            db?.execSQL("alter table Book add column category_id integer")
+        }
     }
 }
